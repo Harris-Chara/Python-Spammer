813613be76befd2acae2599991eece5cc9438ecf
s3742332 <h.chara3@gmail.com>
2021-07-04 21:15:20 +1000

    put email sending in loop, added counters

--- spammer.py
+++ spammer.py
@@ -13,14 +13,26 @@ session = smtplib.SMTP('smtp.gmail.com', 587)
 session.starttls()
 session.login(sender, password)
 
-message = MIMEMultipart()
-message['Subject'] = "I am a spam email"
-message['From'] = sender
-message['To'] = target
-body = 'I am spam'
+#Put in a numerical quantification of how much you hate the person
+hatredQuantified = #<------ENTER NUMBER HERE-------
 
-bodyText = MIMEText(body)
-message.attach(bodyText)
+sentEmails = 1
+while sentEmails <= hatredQuantified:
+    message = MIMEMultipart()
+    message['Subject'] = "I am spam email " + str(sentEmails) + " of " + str(hatredQuantified)
+    message['From'] = sender
+    message['To'] = target
+    body = """Hello,
+    I am an automated email dedicated to spamming you :)
+    Hope you have a wonderful day!
+    
+    Kind Regards"""
 
-session.sendmail(sender, target, message.as_string())
+    bodyText = MIMEText(body)
+    message.attach(bodyText)
+
+    session.sendmail(sender, target, message.as_string())
+    print('Email Number ' + str(sentEmails) + ' has been sent')
+    sentEmails = sentEmails + 1
+
 session.quit()
